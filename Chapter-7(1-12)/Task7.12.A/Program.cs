f5c2e7c73b18e0efd665a66170eb6a4db90b722e
Slavena Georgieva <44895751+slawenna@users.noreply.github.com>
2018-12-03 20:42:21 +0200

    Add files via upload

---
+++ Chapter-7(1-12)/Task7.12.A/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._12.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========A==========");
+            Console.Write("Enter height: ");
+
+            int y = Int32.Parse(Console.ReadLine());
+
+
+
+            Console.Write("Enter width: ");
+
+            int x = Int32.Parse(Console.ReadLine());
+
+
+
+            int a = 0;
+
+
+
+            for (int i = 1; i <= y; i++)
+
+            {
+
+                Console.Write("{0} ", i);
+
+
+
+                a += i;
+
+
+
+                for (int j = 1; j < x; j++)
+
+                {
+
+                    a += y;
+
+                    Console.Write("{0} ", a);
+
+                }
+
+
+
+                a = 0;
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
